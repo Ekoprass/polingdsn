72db9be91d39f92e2401575744d9c78d5a3a5c4a
feerdiana <ferdianarc@gmail.com>
2018-10-12 09:45:46 +0700

    Update application/views/admin/dashboard/mahasiswa.php
    detail mahasiswa 1 kotak

--- application/views/admin/dashboard/mahasiswa.php
+++ application/views/admin/dashboard/mahasiswa.php
@@ -12,33 +12,31 @@
 			<div class="row">	    
 				<div class="col-sm-3">
 	                <div class="panel fresh-color panel-danger">
-		             	<div class="panel-heading"><i class="icon fa fa-th-large fa-2x"> Program Studi</i></div>
+		             	<div class="panel-heading"><i class="icon fa fa-th-large fa-2x"> Detail Mahasiswa</i></div>
 	                   	<div class="panel-body">
+	                   		<echo> Program Studi : 
+	                   	</echo>
 		                    	<div class="sub-title" align="center"><H3><b><?php $id=$row->id_kelas;
 		                       $kelas_siswa=$this->m_polling->kelas_siswa($id)->row_array(); 
 		                       echo $kelas_siswa['nama_prodi'];?></b></H3>
+		                       <div class="panel-body">
+		                       	<echo> Tahun Ajaran : 
+	                   	</echo>
+	                    	<div class="sub-title" align="center"><H3><b><?php $id=$row->id_kelas;
+	                       $kelas_siswa=$this->m_polling->kelas_siswa($id)->row_array(); 
+	                       echo $kelas_siswa['tahun'];?></b></H3></div>
+	                        <div class="panel-body">
+	                        	<echo> Kelas : 
+	                   	</echo>
+								<div class="sub-title" align="center"><H3><b><?php echo $row->id_kelas?></b></H3></div>			                       
+	                    </div>
+                		</div>
 	                		</div>
                 		</div>
 	            	</div>		            
          		</div>
          		<div class="col-sm-3">
-	                <div class="panel fresh-color panel-warning">
-	                    <div class="panel-heading"><i class="icon fa fa-calendar fa-2x"> Tahun Akademik</i></div>
-                   		<div class="panel-body">
-	                    	<div class="sub-title" align="center"><H3><b><?php $id=$row->id_kelas;
-	                       $kelas_siswa=$this->m_polling->kelas_siswa($id)->row_array(); 
-	                       echo $kelas_siswa['tahun'];?></b></H3></div>
-                		</div>
-	            	</div>			            
-         		</div>
 				<div class="col-sm-3">
-	                <div class="panel fresh-color panel-success">
-	                    <div class="panel-heading"><i class="icon fa fa-file fa-2x"> Kelas Semester</i> </div>
-	                    <div class="panel-body">
-								<div class="sub-title" align="center"><H3><b><?php echo $row->id_kelas?></b></H3></div>			                       
-	                    </div>
-	                
-	                </div>
 	            </div>        
 	            <div class="col-sm-3">
 	                <div class="panel fresh-color panel-info">
